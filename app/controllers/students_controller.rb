ad2520813b53d090c99ea41baae96bb1c39f52ef
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-03 11:59:38 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,30 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
+
+  # def activate
+  #   @student = Student.find(params[:id])
+  # end
+
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @student.update(active: true)
+    else
+      @student.update(active: false)
+    end
+    
+    @student.save
+    redirect_to "/students/#{@student.id}"
+  end
+
 
   private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Randy", last_name: "Herasme")
+Student.create(first_name: "Alta", last_name: "Markley")
+Student.create(first_name: "Regis", last_name: "Herasme")
+Student.create(first_name: "Thomas", last_name: "Minaya")
